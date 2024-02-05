1bf8953d72b44cd365aef62d1af006b49de66121
wenpeng <3446459819@qq.com>
2024-02-05 16:43:35 +0800

    step4

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -37,6 +37,14 @@ public interface EmployeeMapper {
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     /**
+     * 通过id查询员工信息
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
+
+    /**
      * 更新员工信息
      * @param employee
      */
